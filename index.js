4a1cdeea61da6fd5cabbd1372ae450c117200946
nintendobruh <nintendobro164@gmail.com>
2020-10-06 16:22:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,124 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const firstMessage = require('./first-message')
+const roleClaim = require('./role-claim')
+const command = require('./command')
+
+client.on('ready', () => {
+    console.log('dude bot is online!')
+
+    roleClaim(client)
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!')
+    })
+
+    command(client, 'servers', message => {
+        client.guilds.cache.forEach(guild => {
+            message.channel.send(`**${guild.name}** has a total of **${guild.memberCount} members.**`)
+        })
+    })
+
+    command(client, ['cc', 'clearchannel'], message => {
+        const { member } = message
+        const tag = `<@${member.id}>`
+
+        if (message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.messages.fetch().then(results => {
+                message.channel.bulkDelete(results)
+            })
+        } else {
+            message.channel.send(`${tag}, you don't have permission to use this command!`)
+        }
+    })
+
+    command(client, 'ban', message => {
+        const { member, mentions } = message
+
+        const tag = `<@${member.id}>`
+
+        if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')) {
+            const target = mentions.users.first()
+
+            if (target) {
+                const targetMember = message.guild.members.cache.get(target.id)
+                if (member.bannable) {
+                targetMember.ban()
+                message.channel.send(`${tag}, ${targetMember} has been banned successfully.`)
+             } else {
+                 message.channel.send(`${tag}, I can't ban this user!`)
+             }} else {
+                message.channel.send(`${tag}, please mention a user you wish to ban!`)
+            }
+        } else {  
+            message.channel.send(
+                `${tag}, you do not have permission to use this command!`
+            )
+        }
+    })
+
+    command(client, 'serverinfo', (message) => {
+        const { guild } = message
+
+        const { name, region, memberCount, owner, afkTimeout, createdAt, rulesChannel } = guild
+        const icon = guild.iconURL()
+
+        const embed = new Discord.MessageEmbed()
+            .setColor(3447003)
+            .setTitle(`Server info for **${name}**`)
+            .setThumbnail(icon)
+            .addFields({
+                name: 'Created on',
+                value: createdAt
+            }, {
+                name: 'Region',
+                value: region
+            }, {
+                name: 'Owner',
+                value: owner
+            }, {
+                name: 'Rules Channel',
+                value: rulesChannel
+            }, {
+                name: 'Member Count',
+                value: memberCount
+            }, {
+                name: 'AFK Timeout',
+                value: afkTimeout
+            })
+
+        message.channel.send(embed)
+    })
+
+    command(client, 'help', message => {
+        const prefix = (config.prefix)
+        const helpembed = new Discord.MessageEmbed()
+        .setColor(3447003)
+        .setTitle(`dude bot commands`)
+        .addFields({
+            name: `Prefix: ${prefix}`,
+            value: '- can be changed.'
+        }, {
+            name: 'General Commands:',
+            value: `
+            ping
+            `
+        }, {
+            name: 'Help Commands:',
+            value: `
+            help, serverinfo
+            `
+        }, {
+            name: 'Administrative Commands:',
+            value: `
+            cc/clearchannel
+            `
+        })
+
+        message.channel.send(helpembed)
+    })
+})
+
+client.login(config.token)
---
+++ role-claim.js
@@ -0,0 +1,66 @@
+const { RichPresenceAssets } = require('discord.js')
+const firstMessage = require('./first-message')
+
+module.exports = client => {
+    const channelId = '762431549727571979'
+
+    const getEmoji = emojiName => client.emojis.cache.find(emoji => emoji.name === emojiName)
+
+    const emojis = {
+        toystory: 'Toy Story Dubs Ping',
+        other: 'Other Dubs Ping',
+        announcement: 'Announcement Ping',
+        video: 'Video Ping',
+        halloffame: 'Hall of Fame Ping'
+    }
+
+    const reactions = []
+
+    let emojiText = '**Please choose a role ping!**\n\n'
+    for (const key in emojis){
+        const emoji = getEmoji(key)
+        reactions.push(emoji)
+
+        const role = emojis[key]
+        emojiText += `${emoji} = ${role}\n`
+    }
+
+
+    firstMessage(client, channelId, emojiText, reactions)
+
+    const handleReaction = (reaction, user, add) => {
+        if (user.id === '755769003804328076') {
+          return
+        }
+
+        const emoji = reaction._emoji.name
+
+        const { guild } = reaction.message
+
+        const roleName = emojis[emoji]
+        if (!roleName) {
+            return
+        }
+
+        const role = guild.roles.cache.find(role => role.name === roleName)
+        const member = guild.members.cache.find(member => member.id === user.id)
+
+        if (add) {
+            member.roles.add(role)
+        } else {
+            member.roles.remove(role)
+        }
+    }
+
+    client.on('messageReactionAdd', (reaction, user) => {
+        if (reaction.message.channel.id === channelId) {
+            handleReaction(reaction, user, true)
+        }
+    })
+
+    client.on('messageReactionRemove', (reaction, user) => {
+        if (reaction.message.channel.id === channelId) {
+            handleReaction(reaction, user, false)
+        }
+    })
+}
