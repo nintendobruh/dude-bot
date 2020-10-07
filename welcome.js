827e76eeef307263f6cba3f5a7d11e5fe1f6fc5d
nintendobruh <nintendobro164@gmail.com>
2020-10-07 13:23:29 -0500

    Per-server welcome message

--- welcome.js
+++ welcome.js
@@ -1,15 +1,76 @@
-module.exports = client => {
-    const channelId = '762414876726525953'
-    const rulesId = '762412313415385138' 
+const mongo = require('./mongo')
+const command = require('./command')
+const welcomeSchema = require('./schemas/welcome-schema')
+
+module.exports = (client) => {
+    const cache = {}
+
+    command(client, 'setwelcome', async (message) => {
+        const { member, channel, content, guild } = message
+
+        if (!member.hasPermission('ADMINISTRATOR')) {
+            channel.send(`<@${member.id}>, you do not have permission to use this command!`)
+            return
+        }
+
+        let text = content
+
+        const split = text.split(' ')
+
+        if (split.length < 2) {
+            channel.send(`<@${member.id}>, please provide a welcome message!`)
+            return
+        }
+
+        split.shift()
+        text = split.join(' ')
+
+        cache[guild.id] = [channel.id, text]
+
+        await mongo().then(async (mongoose) => {
+            try {
+                await welcomeSchema.findOneAndUpdate({
+                    _id: guild.id
+                }, {
+                    _id: guild.id,
+                    channelId: channel.id,
+                    text,
+                }, {
+                    upsert: true
+                })
+            } finally {
+                mongoose.connection.close()
+            }
+        })
+    })
+
+    const onJoin = async member => {
+        const { guild } = member
+
+        let data = cache[guild.id]
+
+        if (!data) {
+
+
+            await mongo().then(async (mongoose) => {
+                try {
+                    const result = await welcomeSchema.findOne({ _id: guild.id })
+
+                    cache[guild.id] = data = [result.channelId, result.text]
+                } finally {
+                    mongoose.connection.close()
+                }
+            })
+        }
+
+        const channelId = data[0]
+        const text = data[1]
+
+        const channel = guild.channels.cache.get(channelId)
+        channel.send(text.replace(/<@>/g, `<@${member.id}>`))
+    }
 
     client.on('guildMemberAdd', member => {
-        const message = `Welcome, <@${
-            member.id
-        }>! Please read ${member.guild.channels.cache
-            .get(rulesId)
-            .toString()}, and proceed at your own risk.`
-
-        const channel = member.guild.channels.cache.get(channelId)
-        channel.send(message)
+        onJoin(member)
     })
 }
